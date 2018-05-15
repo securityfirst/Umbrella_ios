5ed6b2b4e8d213e0c12b03cfa22f0710298fedb4
Lucas Correa <contato@lucascorrea.com>
2018-05-15 10:42:55 +0100

    Added structure MVVM initial

---
+++ Umbrella/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Umbrella
+//
+//  Created by Lucas Correa on 15/05/2018.
+//  Copyright © 2018 Security First. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
