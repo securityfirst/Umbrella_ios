0d968dc17971a3a838e11cedccfb9aae3db6b54a
Lucas Correa <contato@lucascorrea.com>
2018-05-16 00:05:54 +0100

    Added swiftlint.xml and integrated with Fabric

--- Umbrella/AppDelegate/AppDelegate.swift
+++ Umbrella/AppDelegate/AppDelegate.swift
@@ -7,15 +7,19 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        //Fabric
+        Fabric.with([Crashlytics.self])
+        
         return true
     }
 
@@ -41,6 +45,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
 }
-
